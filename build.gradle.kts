5784721060b92e992115284b7ec12448ce612ebc
Bends1 <rusya.shpak123@gmail.com>
2024-05-11 23:10:05 +0300

    123

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    kotlin("jvm") version "1.9.0"
+    kotlin("plugin.serialization") version "1.9.24"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(8)
+}
